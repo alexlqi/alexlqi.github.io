5892291466f40e06671598a62b49eb4e92f66d01
alexlqi <alexlqi@users.noreply.github.com>
2021-01-25 12:41:33 -0600

    update

--- tarea2/app.js
+++ tarea2/app.js
@@ -1,4 +1,5 @@
 // cargamos los datos de PIB estatales por año
+var graf,ancho_total,alto_total,margins,ancho,alto,svg,g,y,x,color,xAxisGroup,yAxisGroup,titulo
 metricaSelect = d3.select('#metrica')
 metrica2 = metricaSelect.node().value
 Anio=2003
@@ -71,9 +72,7 @@ function frame2(Anio){
 
   titulo.text(getTitulo() + Anio + ')')
 
-  console.log(metrica2)
   dataframe.sort((a, b) => {
-    console.log(d3.descending(a[metrica2], b[metrica2]))
     return d3.descending(a[metrica2], b[metrica2])
   })
 
@@ -149,58 +148,11 @@ function render2(data) {
             .attr('x', '-8px')
             .attr('y', '-5px')
             .attr('text-anchor', 'end')
-            .attr('transform', 'rotate(-90)')
+            .attr('transform', 'rotate(-70)')
 }
 
 // I. Configuración
-graf = d3.select('#graf')
-
-ancho_total = graf.style('width').slice(0, -2)
-alto_total = ancho_total * 9 / 16
-
-graf.style('width', `${ ancho_total }px`)
-    .style('height', `${ alto_total }px`)
-
-margins = { top: 30, left: 90, right: 15, bottom: 130 }
-
-ancho = ancho_total - margins.left - margins.right
-alto  = alto_total - margins.top - margins.bottom
-
-// II. Variables globales
-svg = graf.append('svg')
-          .style('width', `${ ancho_total }px`)
-          .style('height', `${ alto_total }px`)
-
-g = svg.append('g')
-        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
-        .attr('width', ancho + 'px')
-        .attr('height', alto + 'px')
-
-y = d3.scaleLinear()
-          .range([alto, 0])
-
-x = d3.scaleBand()
-      .range([0, ancho])
-      .paddingInner(0.1)
-      .paddingOuter(0.3)
-
-color = d3.scaleOrdinal()
-          // .range(['red', 'green', 'blue', 'yellow'])
-          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
-          .range(d3.schemeDark2)
-
-xAxisGroup = g.append('g')
-              .attr('transform', `translate(0, ${ alto })`)
-              .attr('class', 'eje')
-yAxisGroup = g.append('g')
-              .attr('class', 'eje')
-
-titulo = g.append('text')
-          .attr('x', `${ancho / 8}px`)
-          .attr('y', '2.5rem')
-          .attr('text-anchor', 'right')
-          .text('PIB estatal anual')
-          .attr('class', 'titulo-grafica')
+config()
 
 dataArray = []
 
@@ -217,8 +169,6 @@ ascendente = false
 // III. render (update o dibujo)
 
 // IV. Carga de datos
-
-
 regionSelect.on('change', () => {
   region = regionSelect.node().value
   reset()
@@ -232,4 +182,84 @@ metricaSelect.on('change', () => {
 function cambiaOrden() {
   ascendente = !ascendente
   reset()
+}
+
+window.addEventListener('resize', ()=>{
+  config()
+  frame2(Anio)
+});
+
+function config(){
+  var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+  graf = d3.select('#graf')
+  contenedor = d3.select('#contenedor')
+
+  ancho_total = contenedor.style('width').slice(0, -2) - contenedor.style('padding-left').slice(0, -2)*2
+  alto_total = ancho_total * 9 / 16
+
+
+  graf.style('width', `${ ancho_total }px`)
+      .style('height', `${ alto_total }px`)
+
+  switch(orientation){
+    case 'landscape-primary':
+    case 'landscape-secondary':
+      margins = { top: 30, left: 60, right: 15, bottom: 130 }
+      fontSize = '1.5em'
+    break
+    case 'portrait-secondary':
+    case 'portrait-primary':
+      margins = { top: 30, left: 60, right: 15, bottom: 130 }
+      fontSize = '1.1em'
+    break
+    case 'undefined':
+    default:
+      margins = { top: 30, left: 50, right: 15, bottom: 130 }
+      fontSize = '1em'
+    break
+  }
+
+  ancho = ancho_total - margins.left - margins.right
+  alto  = alto_total - margins.top - margins.bottom
+
+  // II. Variables globales
+  if(typeof svg == "object") svg.remove()
+
+  svg = graf.append('svg')
+            .style('width', `${ ancho_total }px`)
+            .style('height', `${ alto_total }px`)
+
+
+  g = svg.append('g')
+          .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
+          .attr('width', ancho + 'px')
+          .attr('height', alto + 'px')
+
+  y = d3.scaleLinear()
+            .range([alto, 0])
+
+  x = d3.scaleBand()
+        .range([0, ancho])
+        .paddingInner(0.1)
+        .paddingOuter(0.3)
+
+  color = d3.scaleOrdinal()
+            // .range(['red', 'green', 'blue', 'yellow'])
+            // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
+            .range(d3.schemeDark2)
+
+  xAxisGroup = g.append('g')
+                .attr('transform', `translate(0, ${ alto })`)
+                .attr('class', 'eje')
+  yAxisGroup = g.append('g')
+                .attr('class', 'eje')
+
+  titulo = g.append('text')
+            //.attr('x', `${ancho / 8}px`)
+            .attr('x', `0px`)
+            .attr('y', '0')
+            .attr('text-anchor', 'right')
+            .text('PIB estatal anual')
+            .attr('class', 'titulo-grafica')
+            .style('font-size',fontSize)
 }
