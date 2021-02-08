e39a83ee0697c5a4311adc2f5ee81c4c5242921a
alexlqi <alexlqi@users.noreply.github.com>
2021-02-08 15:32:04 -0600

    Update app.js

--- ig2/js/app.js
+++ ig2/js/app.js
@@ -1,4 +1,5 @@
 // I. Configuración
+var bars;
 graf = d3.select('#graf')
 
 ancho_total = graf.style('width').slice(0, -2)
@@ -97,232 +98,236 @@ function selectData(elem){
 	d3.select('.descripcion2.'+btn.dataset.ctrl).classed('show',true)
 
 	console.log(d3.select('.descripcion1.'+btn.dataset.ctrl));
-	if(typeof svg == "object") svg.remove()
-	switch(btn.dataset.ctrl){
-		case 'index':
-			svg = graf.append('svg')
-          .style('width', `${ ancho_total }px`)
-          .style('height', `${ alto_total }px`)
+	if(typeof svg == "object"){
+		d3.select("svg").transition().duration(1000).style("opacity","0").on("end", function(){
+			svg.remove()
+			switch(btn.dataset.ctrl){
+				case 'index':
+					svg = graf.append('svg')
+				          .style('width', `${ ancho_total }px`)
+				          .style('height', `${ alto_total }px`)
 
-			g = svg.append('g')
-			        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
-			        .attr('width', ancho + 'px')
-			        .attr('height', alto + 'px')
+					g = svg.append('g')
+					        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
+					        .attr('width', ancho + 'px')
+					        .attr('height', alto + 'px')
 
-			y = d3.scaleLinear()
-			          .range([alto, 0])
+					y = d3.scaleLinear()
+					          .range([alto, 0])
 
-			x = d3.scaleBand()
-			      .range([0, ancho])
-			      //
-			      .paddingInner(0.3)
-			      .paddingOuter(0.3)
+					x = d3.scaleBand()
+					      .range([0, ancho])
+					      //
+					      .paddingInner(0.3)
+					      .paddingOuter(0.3)
 
-			color = d3.scaleOrdinal()
-			           //.range(['red', 'green', 'blue', 'yellow'])
-			          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
-			          .range(d3.schemeDark2)
-
-
-			xAxisGroup = g.append('g')
-			             .attr('transform', `translate(0, ${ alto })`)
-			             // .attr('class', 'eje')
-			yAxisGroup = g.append('g')
-			            //  .attr('class', 'eje')
+					color = d3.scaleOrdinal()
+					           //.range(['red', 'green', 'blue', 'yellow'])
+					          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
+					          .range(d3.schemeDark2)
 
 
-			titulo = g.append('text')
-			          .attr('x', `${ancho / 2}px`)
-			          .attr('y', '-5px')
-			          .attr('text-anchor', 'middle')
-			          .text('Sismos mayores a 7.5 grados de 1900-2020')
-			          //
+					xAxisGroup = g.append('g')
+					             .attr('transform', `translate(0, ${ alto })`)
+					             // .attr('class', 'eje')
+					yAxisGroup = g.append('g')
+					            //  .attr('class', 'eje')
 
-			dataArray = []
-			// IV. Carga de datos
-			d3.csv('sismos0.csv')
-			.then(function(data) {
-			  console.log(data)
-			  data.forEach(d => {
-			    d.latitud = +d.latitud
-			    d.longitud = +d.longitud
-			    d.profundidad = +d.profundidad
-			    d.magnitud   = +d.magnitud
-			    d.profundidad = +d.profundidad
-			    d.mes = +d.mes
-			    d.ano = +d.ano
-			    d.hora = +d.hora
-			    d.minuto = +d.minuto
-			    d.id = +d.id
-			    // damos formato como números a los valores del archivo CSV
-			    })
 
-			     // data = data.slice(0,20)
+					titulo = g.append('text')
+					          .attr('x', `${ancho / 2}px`)
+					          .attr('y', '-5px')
+					          .attr('text-anchor', 'middle')
+					          .text('Sismos mayores a 7.5 grados de 1900-2020')
+					          //
 
-			    this.dataArray = data
-			    maxy = d3.max(data, d => d.magnitud)
-			    y.domain([0, maxy])
-			    x.domain(data.map(d => d.lugar))
-			    color.domain(data.map(d => d.estado))
+					dataArray = []
+					// IV. Carga de datos
+					d3.csv('sismos0.csv')
+					.then(function(data) {
+					  console.log(data)
+					  data.forEach(d => {
+					    d.latitud = +d.latitud
+					    d.longitud = +d.longitud
+					    d.profundidad = +d.profundidad
+					    d.magnitud   = +d.magnitud
+					    d.profundidad = +d.profundidad
+					    d.mes = +d.mes
+					    d.ano = +d.ano
+					    d.hora = +d.hora
+					    d.minuto = +d.minuto
+					    d.id = +d.id
+					    // damos formato como números a los valores del archivo CSV
+					    })
 
-			      // V. Despliegue
-			    render0(dataArray)
-			}).catch( e =>{
-			    console.log('No se tuvo acceso al archivo' + e.message)
-			})
-		break
-		case 'destructivos':
-			svg = graf.append('svg')
-			          .style('width', `${ ancho_total }px`)
-			          .style('height', `${ alto_total }px`)
+					     // data = data.slice(0,20)
 
-			g = svg.append('g')
-			        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
-			        .attr('width', ancho + 'px')
-			        .attr('height', alto + 'px')
+					    this.dataArray = data
+					    maxy = d3.max(data, d => d.magnitud)
+					    y.domain([0, maxy])
+					    x.domain(data.map(d => d.lugar))
+					    color.domain(data.map(d => d.estado))
 
-			y = d3.scaleLinear()
-			          .range([alto, 0])
+					      // V. Despliegue
+					    render0(dataArray)
+					}).catch( e =>{
+					    console.log('No se tuvo acceso al archivo' + e.message)
+					})
+				break
+				case 'destructivos':
+					svg = graf.append('svg')
+					          .style('width', `${ ancho_total }px`)
+					          .style('height', `${ alto_total }px`)
 
-			// a. Para poner las barras vamos a utilizar un scaleBand()
-			x = d3.scaleBand()
-			      .range([0, ancho])
-			      // d. Parámetros extras del escalador
-			      .paddingInner(0.1)
-			      .paddingOuter(0.3)
-			      
-			color = d3.scaleOrdinal()
-			           .range(['#660000', '#990000','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CC0000'])
-			          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
-			          //.range(d3.schemeDark2)
+					g = svg.append('g')
+					        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
+					        .attr('width', ancho + 'px')
+					        .attr('height', alto + 'px')
 
-			// i. Ejes lo primero es crear un "grupo"
-			xAxisGroup = g.append('g')
-			              .attr('transform', `translate(0, ${ alto })`)
-			              .attr('class', 'eje')
-			yAxisGroup = g.append('g')
-			              .attr('class', 'eje')
+					y = d3.scaleLinear()
+					          .range([alto, 0])
 
-			// j. Título superior de la gráfica
-			titulo = g.append('text')
-			          .attr('x', `${ancho / 2}px`)
-			          .attr('y', '-5px')
-			          .attr('text-anchor', 'middle')
-			          .text('La profundidad de los sismos de mayor magnitud en México')
-			          .attr('class', 'titulo-grafica')
+					// a. Para poner las barras vamos a utilizar un scaleBand()
+					x = d3.scaleBand()
+					      .range([0, ancho])
+					      // d. Parámetros extras del escalador
+					      .paddingInner(0.1)
+					      .paddingOuter(0.3)
+					      
+					color = d3.scaleOrdinal()
+					           .range(['#660000', '#990000','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CC0000'])
+					          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
+					          //.range(d3.schemeDark2)
 
-			dataArray = []
+					// i. Ejes lo primero es crear un "grupo"
+					xAxisGroup = g.append('g')
+					              .attr('transform', `translate(0, ${ alto })`)
+					              .attr('class', 'eje')
+					yAxisGroup = g.append('g')
+					              .attr('class', 'eje')
 
-			d3.csv('sismos3.csv')
-			.then(function(data) {
-			  data.forEach(d => {
-			    d.profundidad = +d.profundidad
-			    d.ano = +d.ano
-			    d.puesto = +d.puesto
-			  })
+					// j. Título superior de la gráfica
+					titulo = g.append('text')
+					          .attr('x', `${ancho / 2}px`)
+					          .attr('y', '-5px')
+					          .attr('text-anchor', 'middle')
+					          .text('La profundidad de los sismos de mayor magnitud en México')
+					          .attr('class', 'titulo-grafica')
 
-			  // e. Seleccionar los sismos mas altos
-			  data = data.slice(0, 18)
+					dataArray = []
 
-			  this.dataArray = data
+					d3.csv('sismos3.csv')
+					.then(function(data) {
+					  data.forEach(d => {
+					    d.profundidad = +d.profundidad
+					    d.ano = +d.ano
+					    d.puesto = +d.puesto
+					  })
 
-			  // Calcular la profundidad más alta dentro de
-			  // los datos (columna "profundidad")
-			  maxy = d3.max(data, d => d.profundidad)
+					  // e. Seleccionar los sismos mas altos
+					  data = data.slice(0, 18)
 
-			  // Creamos una función para calcular la altura
-			  // de las barras y que quepan en nuestro canvas
-			  y.domain([0, maxy])
+					  this.dataArray = data
 
-			  // b. Poner el dominio del escalador x, convertir las fechas de los
-			  //    sismos en valores de X para ubicar las barras
-			  x.domain(data.map(d => d.fechamag))
+					  // Calcular la profundidad más alta dentro de
+					  // los datos (columna "profundidad")
+					  maxy = d3.max(data, d => d.profundidad)
 
-			  // V. Despliegue
-			  render2(dataArray)
-			})
-			.catch(e => {
-			  console.log('No se tuvo acceso al archivo ' + e.message)
-			})
-		break
-		case 'profundidad':
-			svg = graf.append('svg')
-			          .style('width', `${ ancho_total }px`)
-			          .style('height', `${ alto_total }px`)
+					  // Creamos una función para calcular la altura
+					  // de las barras y que quepan en nuestro canvas
+					  y.domain([0, maxy])
 
-			g = svg.append('g')
-			        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
-			        .attr('width', ancho + 'px')
-			        .attr('height', alto + 'px')
+					  // b. Poner el dominio del escalador x, convertir las fechas de los
+					  //    sismos en valores de X para ubicar las barras
+					  x.domain(data.map(d => d.fechamag))
 
-			y = d3.scaleLinear()
-			          .range([alto, 0])
+					  // V. Despliegue
+					  render2(dataArray)
+					})
+					.catch(e => {
+					  console.log('No se tuvo acceso al archivo ' + e.message)
+					})
+				break
+				case 'profundidad':
+					svg = graf.append('svg')
+					          .style('width', `${ ancho_total }px`)
+					          .style('height', `${ alto_total }px`)
 
-			// a. Para poner las barras vamos a utilizar un scaleBand()
-			x = d3.scaleBand()
-			      .range([0, ancho])
-			      // d. Parámetros extras del escalador
-			      .paddingInner(0.1)
-			      .paddingOuter(0.3)
-			      
-			color = d3.scaleOrdinal()
-			           .range(['#7FB3D5', '#5DADE2','#34495E'])
-			          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
-			          //.range(d3.schemeDark2)
+					g = svg.append('g')
+					        .attr('transform', `translate(${ margins.left }, ${ margins.top })`)
+					        .attr('width', ancho + 'px')
+					        .attr('height', alto + 'px')
 
-			// i. Ejes lo primero es crear un "grupo"
-			xAxisGroup = g.append('g')
-			              .attr('transform', `translate(0, ${ alto })`)
-			              .attr('class', 'eje')
-			yAxisGroup = g.append('g')
-			              .attr('class', 'eje')
+					y = d3.scaleLinear()
+					          .range([alto, 0])
 
-			// j. Título superior de la gráfica
-			titulo = g.append('text')
-			          .attr('x', `${ancho / 2}px`)
-			          .attr('y', '-5px')
-			          .attr('text-anchor', 'middle')
-			          .text('La profundidad de los sismos en México')
-			          .attr('class', 'titulo-grafica')
+					// a. Para poner las barras vamos a utilizar un scaleBand()
+					x = d3.scaleBand()
+					      .range([0, ancho])
+					      // d. Parámetros extras del escalador
+					      .paddingInner(0.1)
+					      .paddingOuter(0.3)
+					      
+					color = d3.scaleOrdinal()
+					           .range(['#7FB3D5', '#5DADE2','#34495E'])
+					          // https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
+					          //.range(d3.schemeDark2)
 
-			dataArray = []
+					// i. Ejes lo primero es crear un "grupo"
+					xAxisGroup = g.append('g')
+					              .attr('transform', `translate(0, ${ alto })`)
+					              .attr('class', 'eje')
+					yAxisGroup = g.append('g')
+					              .attr('class', 'eje')
 
-			//d3.selectAll('.descripcion1.index').classed('show',false)
-			// IV. Carga de datos
-			d3.csv('sismos1.csv')
-			.then(function(data) {
-			  data.forEach(d => {
-			    d.profundidad = +d.profundidad
-			    d.ano = +d.ano
-			    d.puesto = +d.puesto
-			  })
+					// j. Título superior de la gráfica
+					titulo = g.append('text')
+					          .attr('x', `${ancho / 2}px`)
+					          .attr('y', '-5px')
+					          .attr('text-anchor', 'middle')
+					          .text('La profundidad de los sismos en México')
+					          .attr('class', 'titulo-grafica')
 
-			  // e. Seleccionar los sismos mas altos
-			  data = data.slice(0, 4)
+					dataArray = []
 
-			  this.dataArray = data
+					//d3.selectAll('.descripcion1.index').classed('show',false)
+					// IV. Carga de datos
+					d3.csv('sismos1.csv')
+					.then(function(data) {
+					  data.forEach(d => {
+					    d.profundidad = +d.profundidad
+					    d.ano = +d.ano
+					    d.puesto = +d.puesto
+					  })
 
-			  // Calcular la profundidad más alta dentro de
-			  // los datos (columna "profundidad")
-			  maxy = d3.max(data, d => d.profundidad)
+					  // e. Seleccionar los sismos mas altos
+					  data = data.slice(0, 4)
 
-			  // Creamos una función para calcular la altura
-			  // de las barras y que quepan en nuestro canvas
-			  y.domain([0, maxy])
+					  this.dataArray = data
 
-			  // b. Poner el dominio del escalador x, convertir las fechas de los
-			  //    sismos en valores de X para ubicar las barras
-			  x.domain(data.map(d => d.fechamag))
+					  // Calcular la profundidad más alta dentro de
+					  // los datos (columna "profundidad")
+					  maxy = d3.max(data, d => d.profundidad)
 
-			  // V. Despliegue
-			  render1(dataArray)
-			})
-			.catch(e => {
-			  console.log('No se tuvo acceso al archivo ' + e.message)
-			})
-		break
-		default:
-		break
+					  // Creamos una función para calcular la altura
+					  // de las barras y que quepan en nuestro canvas
+					  y.domain([0, maxy])
+
+					  // b. Poner el dominio del escalador x, convertir las fechas de los
+					  //    sismos en valores de X para ubicar las barras
+					  x.domain(data.map(d => d.fechamag))
+
+					  // V. Despliegue
+					  render1(dataArray)
+					})
+					.catch(e => {
+					  console.log('No se tuvo acceso al archivo ' + e.message)
+					})
+				break
+				default:
+				break
+			}
+		});
 	}
 }
 
